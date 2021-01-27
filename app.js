0482e2eb1b67ba83c03ca3c6f847cfe7602914c3
Kirill <kirill.kirill2@yandex.ru>
2021-01-28 03:20:54 +0500

    Deploy fix

--- app.js
+++ app.js
@@ -67,15 +67,6 @@ app.use(errorLogger);
 
 app.use(errors());
 
-app.use((err, req, res, next) => {
-  if (err.status !== '500') {
-    res.status(err.status).send(err.message);
-    return;
-  }
-  res.status(500).send({ message: `${SERVER_ERROR.INTERNAL_SERVER_ERROR}: ${err.message}` });
-  next();
-});
-
 app.listen(PORT, () => {
   console.log(`Приложение запущено, порт ${PORT}`);
 });
